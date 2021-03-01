0c5e5e01448f56890a2276f358b06219ed82b931
Zain Khan <emailzainkhan@gmail.com>
2021-03-01 02:04:52 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
-const path = require('path');
-
 module.exports = {
-  devServer: {
-    host: 'localhost',
-  },
-  pwa: {
-    workboxOptions: {
-      skipWaiting: true,
-    },
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, 'src/'),
-        vue$: path.resolve('./node_modules/vue/dist/vue.runtime.esm-bundler'),
-      },
-    },
-  },
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/snapcode/'
+    : '/'
+}
